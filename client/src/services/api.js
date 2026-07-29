38fa0387e1a5afe56709dae352ade0704a2e7001
vshukla7 <vinayakshukla3001@gmail.com>
2026-07-29 12:54:36 +0530

    feat: build enviroment fixed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://med-cart-inky.vercel.app/api';
 
 // Fallback Initial Data for seamless instant preview
 const MOCK_CATEGORIES = [
